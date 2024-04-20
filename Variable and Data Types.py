ded7741574f82ad9eb2666c2e3a84a545969c5f0
NotSoumya71 <166982991+NotSoumya71@users.noreply.github.com>
2024-04-20 13:52:35 +0530

    Add files via upload

---
+++ Variable and Data Types.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+integer = 7
+print(integer)
+print(type(integer))
+
+
+# In[3]:
+
+
+string = "Hello!"
+print(string)
+print(type(string))
+
+
+# In[4]:
+
+
+boolean = True
+print(boolean)
+print(type(boolean))
+
+
+# In[5]:
+
+
+real = 10
+imaginary = 3
+complex_num = complex(real, imaginary)
+print(complex_num)
+print(type(complex_num))
+
+
+# In[6]:
+
+
+lists = ["Onion", "Tomato", "Lettuce", "Carrot"]
+print(lists)
+print(type(lists))
+
+
+# In[7]:
+
+
+tuples = ("hello", "fruit", "colors" )
+print (tuples)
+print(type(tuples))
+
+
+# In[8]:
+
+
+dict ={
+    "name" : "Soumya",
+    "designation" : "student",
+    "age" : "13" 
+} 
+print(dict)
+print(type(dict))
+
+
+# In[9]:
+
+
+float = 10.5643
+print(float)
+print(type(float))
+
+
+# In[10]:
+
+
+sets = {96, 35, 87, 12, 46}
+print(sets)
+print(type(sets))
+
+
+# In[14]:
+
+
+a = 50
+b = str(a)
+print(b)
+print(type(b))
+
+
+# In[16]:
+
+
+a = "86"
+x = int(a)
+print(x)
+print(type(x))
+
+
+# In[17]:
+
+
+a = True
+b = str(a)
+print(b)
+print(type(b))
+
+
+# In[18]:
+
+
+a = "soumya"
+b = bool(a)
+print(b)
+print(type(b))
+
+
+# In[20]:
+
+
+a = ("french", "english", "hindi")
+print(type(a))
+b = list(a)
+print(b)
+print(type(b))
+
+
+# In[23]:
+
+
+a = {
+    "name":"soumya",
+    "age":"12"
+}
+b = list(a)
+print(b)
+print(type(b))
+
+
+# In[ ]:
+
+
+
+
