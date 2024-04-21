181458883cbba064d94d0e0010393ea9ca4d6772
NotSoumya71 <166982991+NotSoumya71@users.noreply.github.com>
2024-04-21 13:58:28 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Arithmetic operators
+a = 10 
+b = 5
+
+
+# In[2]:
+
+
+print(a+b)
+
+
+# In[3]:
+
+
+a - b
+
+
+# In[4]:
+
+
+a * b
+
+
+# In[5]:
+
+
+a / b 
+
+
+# In[6]:
+
+
+a // b
+
+
+# In[8]:
+
+
+a % b
+
+
+# In[20]:
+
+
+a ** b
+
+
+# In[21]:
+
+
+#Assignment operators
+p = 20
+q = 5
+
+
+# In[23]:
+
+
+p += q
+print(p)
+
+
+# In[24]:
+
+
+p -= q
+print(p)
+
+
+# In[25]:
+
+
+p *= q
+print(p)
+
+
+# In[26]:
+
+
+p /= q
+print(p)
+
+
+# In[27]:
+
+
+p //= q
+print(p)
+
+
+# In[28]:
+
+
+p %= q
+print(p)
+
+
+# In[29]:
+
+
+p **= q
+print(p)
+
+
+# In[1]:
+
+
+#Logical operators
+x = 90
+y = 70
+print(x > 5 and y > 60)
+
+
+# In[2]:
+
+
+print(x > 103 or y <= 80)
+
+
+# In[3]:
+
+
+print(not(x>=80))
+
+
+# In[4]:
+
+
+#Comparison Operators
+r = 34
+s = 34
+print(r==s)
+
+
+# In[5]:
+
+
+print(r!=s)
+
+
+# In[6]:
+
+
+print(r>s)
+
+
+# In[7]:
+
+
+r=32
+print(r<s)
+
+
+# In[8]:
+
+
+print(r>=s)
+
+
+# In[9]:
+
+
+print(r<=s)
+
+
+# In[16]:
+
+
+#Identity operators
+x = [10, 24, 36]
+y = [10, 24, 36]
+print(x is not y)
+
+
+# In[17]:
+
+
+print(x is y)
+
+
+# In[18]:
+
+
+print(x != y)
+
+
+# In[24]:
+
+
+#Membership Operators
+x = [94, 64, 39, 49]
+print(94 in x)
+
+
+# In[25]:
+
+
+print(39 not in x)
+
+
+# In[26]:
+
+
+print(433 in x)
+
+
+# In[ ]:
+
+
+
+
