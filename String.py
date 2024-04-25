606f1a71a66eba352ac51ac38a32f9dce19f773f
NotSoumya71 <166982991+NotSoumya71@users.noreply.github.com>
2024-04-25 11:50:08 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = "math"
+x = a.capitalize()
+print(x)
+#capitalize makes the first alphabet captial.
+
+
+# In[3]:
+
+
+b = "KeYbOArD"
+x = b.casefold()
+print(x)
+#casefold makes everything lowercase.
+
+
+# In[4]:
+
+
+a = "Pineapple"
+x = a.center(80)
+print(x)
+#centers the string, by the value entered as arguement.
+
+
+# In[6]:
+
+
+b = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+x = b.count("wood")
+print(b)
+print(x)
+#count is used to count the number of times a word has appeared.
+
+
+# In[7]:
+
+
+a = "This is an encoded message"
+print(a.encode(encoding="ascii", errors="ignore"))
+#encode is used to encode the text into a different form so that it can't be understood by anyone
+
+
+# In[4]:
+
+
+b = "She loves to read books."
+print(b.endswith("."))
+#Endswith helps in finding out the last character of a sentence.
+
+
+# In[5]:
+
+
+a = "Peter piper picked a peck of pickled peppers."
+print(a.find("peck"))
+#Find is used to locate a particular value/character/word.
+
+
+# In[ ]:
+
+
+
+
