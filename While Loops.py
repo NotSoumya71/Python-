288dc282e5dbc772f3b881747a619198a59e3c20
NotSoumya71 <166982991+NotSoumya71@users.noreply.github.com>
2024-05-06 14:57:25 +0530

    Add files via upload

---
+++ While Loops.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i +=1
+
+
+# In[2]:
+
+
+i = 1
+while i < 7:
+    print(i)
+    if i == 3:
+        break
+    i = i +1
+
+
+# In[3]:
+
+
+i = 1
+while i < 7:
+    i +=1
+    if i == 4:
+        continue 
+    print(i)
+
+
+# In[7]:
+
+
+i = 1
+while i < 7:
+    print(i)
+    i +=1
+else:
+    print("Finished!")
+
+
+# In[18]:
+
+
+i = 3
+while i <= 30:
+    print(i)
+    i = i+3
+
+
+# In[22]:
+
+
+i = 2
+while i % 2 == 0 and i <= 20:
+    print(i)
+    i = i+2
+
+
+# In[26]:
+
+
+i = 1
+while i % 2 != 0 and i <= 30:
+    print(i)
+    i +=2
+
+
+# In[ ]:
+
+
+while True
+i = 1
+
